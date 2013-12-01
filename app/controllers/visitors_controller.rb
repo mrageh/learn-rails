ff162a2b31d714482cf94c9fad15a5327f0f2f03
Adam89 <Adamm1@aston.ac.uk>
2013-11-30 23:24:34 -0700

    zurb front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,7 +3,6 @@ class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
     Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
-    flash.now[:alert] = "Welcome!"
   end
 
 end
